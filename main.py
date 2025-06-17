5ae232fdb3da1fcf39f2ce67692b9cf83d4b357c
noam emilio radetich <noam81@yahoo.it>
2025-06-17 16:31:17 +0200

    implementing the text rendering

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from libraries_and_settings import (pygame,
                                     sys)
 ###CONFIGURATIONS
-from libraries_and_settings import (display_surface, maps, TILE_SIZE)
+from libraries_and_settings import (display_surface, maps, TILE_SIZE, WINDOW_HEIGHT,WINDOW_WIDTH,font)
 
 ###SPRITES
 from player import Player
@@ -22,6 +22,8 @@ class Game:
         self.current_map = None
         self.current_area = "world"
         self.area_group = {}
+        self.transition_bool = True
+        self.transition_bool = True
 
         self.collision_sprites = pygame.sprite.Group()
         self.all_sprites = allSpritesOffset()
@@ -37,16 +39,12 @@ class Game:
         for name, map in self.maps.items():
             if name == self.current_area:
                 self.current_map = map
-
         ###ground
         for x, y, image in self.current_map.get_layer_by_name('ground').tiles():
             GeneralSprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites,True)
-
         ###objects
         for obj in self.current_map.get_layer_by_name('objects'):
             GeneralSprite((obj.x, obj.y), obj.image, (self.all_sprites,self.collision_sprites),None,obj.name,1)
-
-
         ###player
         for obj in self.current_map.get_layer_by_name('areas'):
             if obj.name == 'player_spawn':
@@ -59,11 +57,29 @@ class Game:
             if obj.name not in ('bat', 'scheleton', 'wall', 'flame', 'dragon'):
                 self.area_group[obj.name] = AreaSprite(obj.x, obj.y, obj.width, obj.height, self.all_sprites)
 
+    def enter_area_check(self,event):
+        for name, area in self.area_group.items():
+        ###check if the player pressed yes key to enter the area
+            if (area.rect.colliderect(self.player.rect)
+                    and event.type == pygame.KEYDOWN
+                    and event.key == pygame.K_y):
+                self.transition_bool = True
+
+
     def transition(self):
+        ###determine the current area map to be loaded and print it
         for name, area in self.area_group.items():
             if area.rect.colliderect(self.player.rect):
                 self.current_area = name
-            print(self.current_area)
+                self.text = f"You found a {name} press Y to enter"
+                self.text_surface = font.render(self.text,True,"white")
+                self.display_surface.blit(self.text_surface, (self.player.rect.centerx, self.player.rect.centery))
+
+        ###perform the actual transition between areas
+        if self.transition_bool:
+            self.mapping()
+            self.transition_bool = False
+
 
     def run(self):
         while self.running:
@@ -72,16 +88,17 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
                     sys.exit()
+                self.enter_area_check(event)
 
-            self.display_surface.fill('black')
             self.transition()
+            self.display_surface.fill('black')
             self.all_sprites.update(dt)
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
+
 
         pygame.quit()
 
 if __name__ == '__main__':
     main_game = Game()
-    main_game.mapping()
     main_game.run()
---
+++ words_library.py
@@ -0,0 +1,87 @@
+phrases = {
+    "text_1" : "do you want to sell your crystal balls? \n"
+                                     "Press E to see your tradable resources. S to sell \n"
+                                     "B to buy potions\n"
+                                     "N to buy the holy water"
+    ,
+    "text_2" : "do you want inspect the ",
+    "text_3" : "do you want to collect the energy of the runes?",
+    "text_4" : "You have too much runes energy, you can't collect more",
+    "text_5" : "you collected more than 30 coins you get a potion!",
+    "text_6" : "Coin collection game: \n\n"
+            "Use Arrow Keys to move, SPACE to jump, ESC to exit \n"
+            "get more than 30 coins for a free potion \n\n"
+            "Main game: \n\n"
+            "Use arrows to move the character around \n"
+            "Y to enter areas or inspect objects \n"
+            "Press 1 to use the potion \n"
+            "Press 2 to drink the holy water \n"
+            "Press m to access the menu \n"
+            "Press f to release the runic fire \n"
+            "Press space to cast a rune \n"
+            "Press ESC to return to the game \n"
+            "Press 0 to reset the text collected while solving riddles",
+    "text_7" : "find the solution to the riddle the get the key for the portal",
+    "text_8" : "Press Y to enter the ",
+    "text_9" : "there is an inscription on the stone... are you able to decypher it?"
+}
+
+cipher_dict = {
+    'RVPNC': 'sword',
+    'BZRSKD': 'castle',
+    'CQZFNM': 'dragon',
+    'OPSJNM': 'potion',
+    'RGHKDC': 'shield',
+    'VHZCQC': 'wizard',
+    'RODKKAANNJ': 'spellbook',
+    'SBUQDM': 'tavern',
+    'FNAKHM': 'goblin',
+    'CTFNDNM': 'dungeon',
+    'DKWHJQ': 'elixir',
+    'JMHFGS': 'knight',
+    'NQB': 'orc',
+    'QTMD': 'rune',
+    'BZTKCQN': 'cauldron',
+    'VZQKNBJ': 'warlock',
+    'RNQBDQDRR': 'sorceress',
+    'AZQC': 'bard',
+    'BGZKHBD': 'chalice',
+    'OGNDHW': 'phoenix',
+    'RBPSSK': 'scroll',
+    'DMZMBMSDQ': 'enchanter',
+    'SGQNMDD': 'throne',
+    'VXUDQM': 'wyvern',
+    'BKNZJ': 'cloak',
+    'RBODOSQ': 'scepter',
+    'VHSBG': 'witch',
+    'OZKZCHM': 'paladin',
+    'ZQSGZESBZ': 'artifact',
+    'VQZHSG': 'wraith',
+    'ADZRSZLSSDQ': 'beastmaster',
+    'KZHQ': 'lair',
+    'GDBKDQ': 'healer',
+    'RDDQ': 'seer',
+    'ODFZFTR': 'pegasus',
+    'QHSTZK': 'ritual',
+    'BNUDM': 'coven',
+    'PDTSM': 'quest'
+}
+
+
+areas_description = {
+    "world_map" : " main area",
+    "frozen house" : "careful it is very cold in there!!!",
+    "house" : "looks like an house of a merchant",
+    "abandoned house" : "no one lives here... probably",
+    "alchemist house" : "the signs on the door don't promise any good",
+    "bonus area" : "",
+    "cemetery" : "looks scary",
+    "forest" : "a peaceful place",
+    "maze" : "you can enter, but can you exit?",
+    "secret passage": "it was suppose to be a secret",
+    "spawning area" : "careful to what you wish for...",
+    "store" : "shopping time",
+    "dungeon" : "you will need some potions and all the runic energy you can...",
+    "safe" : ""
+
+}
